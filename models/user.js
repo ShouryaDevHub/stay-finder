0d849f51a4aa25b1b9650209b72e73884da15942
Shourya Rawat <rawatshourya07@gmail.com>
2026-07-13 09:23:56 +0530

    writting some comments for explanation

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  // we didn't write the username and password as passport-local-mongoose automatically added in it also with SALT and stored the password in HASH.
 });
 
 userSchema.plugin(passportLocalMongoose);
