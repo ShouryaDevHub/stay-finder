d7b88f81d42fb657aff262ff41cdab823d9287a3
Shourya Rawat <rawatshourya07@gmail.com>
2026-07-20 14:29:15 +0530

    makking review system more creative and Creating feature of uploading images and storing in Cloudinary.

--- routes/listing.js
+++ routes/listing.js
@@ -3,49 +3,40 @@ const router = express.Router();
 const ExpressError = require("../utils/ExpressError.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
+const multer = require("multer");
+const { storage } = require("../CloudConfig.js");
+const upload = multer({ storage });
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
 
-// INDEX ROUTE =>
-router.get("/", wrapAsync(listingController.index));
+router.route("/").get(wrapAsync(listingController.index)).post(
+  isLoggedIn,
+  upload.single("listing[image][url]"),
+  validateListing, // passing validateListing as a MIDDLEWARE.
+  wrapAsync(listingController.createListing),
+);
 
 // NEW ROUTE
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-// SHOW ROUTE =>
-router.get("/:id", wrapAsync(listingController.showListing));
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing),
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
-// CREATE ROUTE =>
-router.post(
-  "/",
-  isLoggedIn,
-  validateListing, // passing validateListing as a MIDDLEWARE.
-  wrapAsync(listingController.createListing),
-);
 // EDIT ROUTE =>
 router.get(
   "/:id/edit",
   isLoggedIn,
   isOwner,
   wrapAsync(listingController.renderEditForm),
-);
-
-// UPDATE ROUTE =>
-router.put(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsync(listingController.updateListing),
-);
-
-// DELETE ROUTE =>
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingController.destroyListing),
 );
 
 module.exports = router;
